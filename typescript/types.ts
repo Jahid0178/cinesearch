43b27d521c64dfe3042abfa406067427bbfa2829
Jahidul <jahidulhaque767@gmail.com>
2024-10-24 01:15:13 +0600

    chore: add more Movie type properties in types.ts file

--- typescript/types.ts
+++ typescript/types.ts
@@ -13,4 +13,34 @@ export type MovieType = {
   video: boolean;
   vote_average: number;
   vote_count: number;
+  homepage: string;
+  belongs_to_collection: {
+    id: number;
+    name: string;
+    poster_path: string | null;
+    backdrop_path: string;
+  };
+  budget: number;
+  genres: {
+    id: number;
+    name: string;
+  }[];
+  production_companies: {
+    id: number;
+    name: string;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  revenue: number;
+  status: string;
+  tagline: string;
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
 };
