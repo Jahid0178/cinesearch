52790ce508d30f8ca189164829b4f6045358207f
Jahidul <jahidulhaque767@gmail.com>
2024-10-24 18:50:07 +0600

    feat: add all fetcher methods in actions.ts file

---
+++ lib/actions/actions.ts
@@ -0,0 +1,80 @@
+export async function fetchPopularMovies() {
+  try {
+    const response = await fetch("http://localhost:3000/api/popular", {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("fetch_popular_movies", error);
+  }
+}
+
+export async function fetchMovieDetails(movieId: string) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`,
+      {
+        method: "GET",
+      }
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("fetch_movie_details", error);
+  }
+}
+
+export async function fetchMovieCast(movieId: string) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${process.env.TMDB_API_KEY}`
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("fetch_movie_cast", error);
+  }
+}
+
+export async function fetchRecommendationsMovie(movieId: string) {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${process.env.TMDB_API_KEY}`,
+      {
+        method: "GET",
+        next: {
+          revalidate: 60,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("recommendation error", error);
+  }
+}
+
+export async function handleAddToWatchlist(movieId: number) {
+  try {
+    const response = await fetch(`http://localhost:3000/api/watchlist`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ movieId }),
+    });
+
+    console.log(response);
+  } catch (error) {
+    console.log("error", error);
+  }
+}
