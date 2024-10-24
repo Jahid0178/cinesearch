721178c0bc5bd33a7d49e907ed301b1d0634976b
Jahidul <jahidulhaque767@gmail.com>
2024-10-24 21:27:07 +0600

    feat: add watchlist page without content

---
+++ app/(client)/watchlist/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WatchlistPage = () => {
+  return (
+    <section>
+      <div className="container">
+        <p>Watchlist page</p>
+      </div>
+    </section>
+  );
+};
+
+export default WatchlistPage;
